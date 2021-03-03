b26e01449e33a66fbd95a568e880a665743930a6
Fujiokaryo <f.tooru17@gmail.com>
2021-03-03 14:54:24 +0900

    発展20まで

---
+++ Assets/Scripts/BulletDataSO.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "BulletDataSO", menuName = "Create BulletDataSO")]
+public class BulletDataSO : ScriptableObject
+{
+    public List<BulletData> bulletDataList = new List<BulletData>();
+
+    [Serializable]
+    public enum BulletType
+    {
+        A,
+        B,
+        C,
+        Player_Normal,
+        Player_Blaze,
+        Player_3ways_Piercing,
+        Player_5ways_Normal,
+        None,
+    }
+
+    [Serializable]
+    public enum LiberalType
+    {
+        Player,
+        Enemy,
+        Boss
+    }
+
+    [Serializable]
+    public class BulletData
+    {
+        public int no; //バレットの通し番号
+        public float bulletSpeed;  //速度
+        public int bulletPower; //攻撃力
+        public float loadingTime;  //発射までの待機時間
+        public BulletType bulletType; //バレットの種類
+        public Sprite btnSprite;  //バレット選択用のボタンの画像
+        public LiberalType liberalType; //バレットの利用者の種類
+        public int openExp; //バレットを利用するために必要なExp
+        public float launchTime; //バレットを使用できる時間
+        public Sprite bulletSprite; //発射するバレットの画像
+        public string discription; //バレットの説明文
+    }
+}
